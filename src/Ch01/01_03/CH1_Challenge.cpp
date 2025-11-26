c84422a82a32be9eeb68a850bd5b4437991bb910
swampglow <98502756+swampglow@users.noreply.github.com>
2025-11-26 19:26:22 +0000

    My ch 1 challenge

---
+++ src/Ch01/01_03/CH1_Challenge.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string str;
+    std::cout << "Enter your Name:" << std::flush;
+    std::cin >> str;
+    std::cout << "Nice to meet you!, " << str << "!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return (0);
+}
